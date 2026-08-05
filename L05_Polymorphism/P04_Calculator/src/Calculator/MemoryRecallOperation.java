f8f9880818f6cca29fb640e5f315ee15327946f4
YuliyanKastamanov <92514049+YuliyanKastamanov@users.noreply.github.com>
2026-08-05 22:35:32 +0300

    Polymorphism - Exercises 4

---
+++ L05_Polymorphism/P04_Calculator/src/Calculator/MemoryRecallOperation.java
@@ -0,0 +1,22 @@
+package Calculator;
+public class MemoryRecallOperation implements Operation {
+    private MemoryStorage memory;
+
+    public MemoryRecallOperation(MemoryStorage memory) {
+        this.memory = memory;
+    }
+
+    @Override
+    public void addOperand(int operand) {
+    }
+
+    @Override
+   public boolean isCompleted(){
+        return true;
+    }
+
+    @Override
+    public int getResult() {
+        return this.memory.getResult();
+    }
+}
